fe50c5a1d7cd7a31069514b9fb4ffeca47902a87
ashvanee <ashvaneet@hotmail.com>
2025-11-05 23:59:29 +0530

    resolving errors

--- weather/settings.py
+++ weather/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'weather.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'default': dj_database_url.parse(
+    'default': { 
+        dj_database_url.parse(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
